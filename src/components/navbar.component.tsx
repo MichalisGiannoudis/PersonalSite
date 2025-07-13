3e34fb692123659b2f68e418def3a83c7d7561cc
Michalis Giannoudis <michalis221@gmail.com>
2025-07-13 23:24:50 +0300

    navbar ready for mobile

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -92,38 +92,29 @@ export const NavBar = () => {
   };
 
   return (
-    <>
-      <div className={`hidden fixed top-5 left-1/2 transform -translate-x-1/2 pointer-events-auto bg-gray-900/40 md:bg-gray-900/40 w-[300px] md:w-[400px] h-16 rounded-2xl md:rounded-4xl shadow-lg backdrop-blur-md drop-shadow-xl z-50 transition-all duration-700 ease-out ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-20 opacity-0'}`}>
-        <div className="grid grid-cols-3 h-full">
-          <div className={`absolute top-1 left-1 h-7/8 w-[calc(33.333%-0.15rem)] bg-purple-700/50 rounded-3xl transition-all duration-700 ease-in-out ${
-              activeSection === -1 ? 'opacity-0' :
-              activeSection === 0 ? 'translate-x-0 opacity-100' : 
-              activeSection === 1 ? 'translate-x-full opacity-100' : 
-              'translate-x-[200%] opacity-100'}`}/>
-          <div className="cursor-pointer group h-full flex items-center justify-center relative z-10">
-              <p onClick={() => handleSectionClick(0, 'bio-section', '#bio')} 
-                className={`text-lg font-bold relative overflow-hidden flex justify-center items-center cursor-pointer h-full transition-colors duration-700 ${activeSection === 0 ? 'text-white' : 'text-white'}`}>
-                <span className="inline-block from-center-vert transition-transform duration-1000 ease-in-out"> About me</span>
-            </p>
-        </div>
+    <div className={`fixed top-5 left-1/2 transform -translate-x-1/2 pointer-events-auto bg-gray-900/40 md:bg-gray-900/40 w-[300px] md:w-[400px] h-14 md:h-16 rounded-2xl md:rounded-4xl shadow-lg backdrop-blur-md drop-shadow-xl z-50 transition-all duration-700 ease-out ${isVisible ? 'translate-y-0 opacity-100' : '-translate-y-20 opacity-0'}`}>
+      <div className="grid grid-cols-3 h-full">
+        <div className={`absolute top-1 left-1 h-7/8 w-[calc(33.333%-0.15rem)] bg-purple-700/50 rounded-3xl transition-all duration-700 ease-in-out ${
+            activeSection === -1 ? 'opacity-0' : activeSection === 0 ? 'translate-x-0 opacity-100' : activeSection === 1 ? 'translate-x-full opacity-100' : 'translate-x-[200%] opacity-100'}`}/>
         <div className="cursor-pointer group h-full flex items-center justify-center relative z-10">
-            <p onClick={() => handleSectionClick(1, 'portfolio-section', '#portfolio')} 
-                className={`text-lg font-bold relative overflow-hidden flex justify-center items-center cursor-pointer h-full transition-colors duration-700 ${activeSection === 1 ? 'text-white' : 'text-white'}`}>
-                <span className="inline-block from-center-vert transition-transform duration-1000 ease-in-out"> Portfolio</span>
-            </p>
-        </div>
-        <div className="cursor-pointer group h-full flex items-center justify-center relative z-10">
-            <p onClick={() => handleSectionClick(2, 'contact-section', '#contact')} 
-                className={`text-lg font-bold relative overflow-hidden flex justify-center items-center cursor-pointer h-full transition-colors duration-700 ${activeSection === 2 ? 'text-white' : 'text-white'}`}>
-                <span className="inline-block from-center-vert transition-transform duration-1000 ease-in-out">Contact</span>
-            </p>
-        </div>
-        </div>
+            <p onClick={() => handleSectionClick(0, 'bio-section', '#bio')} 
+              className={`text-sm md:text-lg font-bold relative overflow-hidden flex justify-center items-center cursor-pointer h-full transition-colors duration-700 ${activeSection === 0 ? 'text-white' : 'text-white'}`}>
+              <span className="inline-block from-center-vert transition-transform duration-1000 ease-in-out"> About me</span>
+          </p>
       </div>
-      <div className={`block`}>
-
+      <div className="cursor-pointer group h-full flex items-center justify-center relative z-10">
+          <p onClick={() => handleSectionClick(1, 'portfolio-section', '#portfolio')} 
+              className={`text-sm md:text-lg font-bold relative overflow-hidden flex justify-center items-center cursor-pointer h-full transition-colors duration-700 ${activeSection === 1 ? 'text-white' : 'text-white'}`}>
+              <span className="inline-block from-center-vert transition-transform duration-1000 ease-in-out"> Portfolio</span>
+          </p>
       </div>
-    </>
-
+      <div className="cursor-pointer group h-full flex items-center justify-center relative z-10">
+          <p onClick={() => handleSectionClick(2, 'contact-section', '#contact')} 
+              className={`text-sm md:text-lg font-bold relative overflow-hidden flex justify-center items-center cursor-pointer h-full transition-colors duration-700 ${activeSection === 2 ? 'text-white' : 'text-white'}`}>
+              <span className="inline-block from-center-vert transition-transform duration-1000 ease-in-out">Contact</span>
+          </p>
+      </div>
+      </div>
+    </div>
   );
 }
