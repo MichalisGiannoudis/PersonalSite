6ac33b48c486522f3e3963ba3111c4ea0dae537d
Michalis Giannoudis <michalis221@gmail.com>
2025-07-14 13:28:57 +0300

    fixed timeline for mobile

--- src/components/experienceCard.component.tsx
+++ src/components/experienceCard.component.tsx
@@ -7,32 +7,47 @@ interface ExperienceCardProps {
 
 export const ExperienceCard = ({ episode, position }: ExperienceCardProps) => {
     return(
-        <div className={`flex-1 ${position === 'left' ? 'text-right pr-8' : 'pl-8'}`}>
-            <div className="bg-slate-900/20 rounded-lg p-6 border border-slate-700/50 relative">
-                <div className="flex items-center gap-6">
-                    <div className={position === 'left' ? 'w-1/3' : 'w-3/5'}>
-                        {position === 'left' ? (
-                            <>
-                                <h4 className="text-xl font-bold mb-2 text-purple-800">{episode.company}</h4>
-                                <p className="text-lg leading-relaxed mb-3">{episode.role}</p>
-                                <span className="text-sm text-gray-400">{episode.period}</span>
-                            </>
-                        ) : (<p className="text-default leading-relaxed mb-3">{episode.description}</p>)}
-                    </div>
-                    <div className="w-px h-22 bg-gradient-to-b from-purple-800 to-indigo-700"></div>
-                    <div className="flex-1">
-                        {position === 'left' ? (
-                            <p className="text-default leading-relaxed mb-3">{episode.description}</p>) 
-                            : (
-                            <>
-                                <h4 className="text-xl font-bold mb-2 text-purple-800">{episode.company}</h4>
-                                <p className="text-lg leading-relaxed mb-3">{episode.role}</p>
-                                <span className="text-sm text-gray-400">{episode.period}</span>
-                            </>
-                        )}
+        <>
+            <div className="flex lg:hidden ml-6">
+                <div className="bg-slate-900/20 rounded-lg p-2 border border-slate-700/50 relative">
+                    <div className="flex items-center gap-6">
+                        <div>
+                            <h4 className="text-xl font-bold mb-2 text-purple-800">{episode.company}</h4>
+                            <p className="text-lg leading-relaxed mb-3">{episode.role}</p>
+                            <span className="text-sm text-gray-400">{episode.period}</span>
+                            <div className="w-full h-1 mb-2 mt-2 bg-gradient-to-b from-purple-800 to-indigo-700"></div>
+                            <p className="text-default leading-relaxed mb-3">{episode.description}</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div className={`hidden lg:flex flex-1 ${position === 'left' ? 'text-right pr-8' : 'pl-8'}`}>
+                <div className="bg-slate-900/20 rounded-lg p-6 border border-slate-700/50 relative">
+                    <div className="flex items-center gap-6">
+                        <div className={position === 'left' ? 'w-1/3' : 'w-3/5'}>
+                            {position === 'left' ? (
+                                <>
+                                    <h4 className="text-xl font-bold mb-2 text-purple-800">{episode.company}</h4>
+                                    <p className="text-lg leading-relaxed mb-3">{episode.role}</p>
+                                    <span className="text-sm text-gray-400">{episode.period}</span>
+                                </>
+                            ) : (<p className="text-default leading-relaxed mb-3">{episode.description}</p>)}
+                        </div>
+                        <div className="w-px h-22 bg-gradient-to-b from-purple-800 to-indigo-700"></div>
+                        <div className="flex-1">
+                            {position === 'left' ? (
+                                <p className="text-default leading-relaxed mb-3">{episode.description}</p>) 
+                                : (
+                                <>
+                                    <h4 className="text-xl font-bold mb-2 text-purple-800">{episode.company}</h4>
+                                    <p className="text-lg leading-relaxed mb-3">{episode.role}</p>
+                                    <span className="text-sm text-gray-400">{episode.period}</span>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )               
 }
--- src/components/timeline.component.tsx
+++ src/components/timeline.component.tsx
@@ -33,7 +33,7 @@ export const TimeLine = () => {
 
     return (
         <>
-            <div className="text-center mb-16">
+            <div className="text-center mb-10">
                 <div className="flex items-center justify-center mb-6">
                     <p className="text-4xl font-bold mx-4 whitespace-nowrap">My Journey</p>
                 </div>
@@ -43,7 +43,7 @@ export const TimeLine = () => {
                     <div className="h-0.5 flex-1 bg-gradient-to-l from-transparent to-white/40"></div>
                 </div>
             </div>
-            <div className="p-8 relative min-h-[1450px] lg:min-h-[800px] overflow-hidden">            
+            <div className="p-8 relative min-h-[1200px] lg:min-h-[800px] overflow-hidden">            
                 {visualize && (
                     <div className={`absolute inset-0 flex items-center justify-center animate-fade-in-crawl`}>
                         <StarWars isPlaying={visualize}/>
@@ -64,7 +64,7 @@ export const TimeLine = () => {
                 {!visualize && (
                     <div className="flex lg:justify-center relative">
                         {['idle', 'fading', 'shrinking'].includes(animationStep) && (
-                            <div className={`h-335 lg:h-155 w-1 bg-gradient-to-b from-blue-500 to-purple-500 rounded-full ${animationStep === 'shrinking' ? 'animate-shrink-line' : ''}`}></div>
+                            <div className={`h-275 lg:h-155 w-1 bg-gradient-to-b from-blue-500 to-purple-500 rounded-full ${animationStep === 'shrinking' ? 'animate-shrink-line' : ''}`}></div>
                         )}
                         {['idle', 'fading'].includes(animationStep) && (
                             <div className={`absolute inset-0 space-y-12 ${animationStep === 'fading' ? 'animate-fade-out-box' : ''}`}>
@@ -74,11 +74,11 @@ export const TimeLine = () => {
                                             <>
                                                 <ExperienceCard episode={episode} position="left" />
                                                 <div className="absolute ml-0.5 lg:ml-0 lg:left-1/2 transform -translate-x-1/2 w-4 h-4 bg-white rounded-full border-4 border-gray-900"></div>
-                                                <div className="flex-1 pl-4"></div>
+                                                <div className="flex-1 lg:pl-4"></div>
                                             </>
                                         ) : (
                                             <>
-                                                <div className="flex-1 pr-4"></div>
+                                                <div className="flex-1 lg:pr-4"></div>
                                                 <div className="absolute ml-0.5 lg:ml-0 lg:left-1/2 transform -translate-x-1/2 w-4 h-4 bg-white rounded-full border-4 border-gray-900"></div>
                                                 <ExperienceCard episode={episode} position="right" />
                                             </>
