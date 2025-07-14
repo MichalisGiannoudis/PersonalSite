cf6f1579eec3e528023ed91ccc6a02c586eb2825
Michalis Giannoudis <michalis221@gmail.com>
2025-07-14 12:17:50 +0300

    fixed line and bullet of timeline for mobile

--- src/components/timeline.component.tsx
+++ src/components/timeline.component.tsx
@@ -43,7 +43,7 @@ export const TimeLine = () => {
                     <div className="h-0.5 flex-1 bg-gradient-to-l from-transparent to-white/40"></div>
                 </div>
             </div>
-            <div className="p-8 relative min-h-[800px] overflow-hidden">            
+            <div className="p-8 relative min-h-[1450px] lg:min-h-[800px] overflow-hidden">            
                 {visualize && (
                     <div className={`absolute inset-0 flex items-center justify-center animate-fade-in-crawl`}>
                         <StarWars isPlaying={visualize}/>
@@ -62,28 +62,29 @@ export const TimeLine = () => {
                 )}
 
                 {!visualize && (
-                    <div className="relative h-full">
+                    <div className="flex lg:justify-center relative">
                         {['idle', 'fading', 'shrinking'].includes(animationStep) && (
-                            <div className={`absolute left-1/2 transform -translate-x-1/2 w-1 bg-gradient-to-b from-blue-500 to-purple-500 rounded-full h-full ${animationStep === 'shrinking' ? 'animate-shrink-line' : ''}`}></div>
+                            <div className={`h-335 lg:h-155 w-1 bg-gradient-to-b from-blue-500 to-purple-500 rounded-full ${animationStep === 'shrinking' ? 'animate-shrink-line' : ''}`}></div>
                         )}
                         {['idle', 'fading'].includes(animationStep) && (
-                            <div className={`space-y-12 ${animationStep === 'fading' ? 'animate-fade-out-box' : ''}`}>
+                            <div className={`absolute inset-0 space-y-12 ${animationStep === 'fading' ? 'animate-fade-out-box' : ''}`}>
                                 {episodes.map((episode, index) => (
-                                    <div key={episode.company} className="relative flex items-center">
+                                    <div key={episode.company} className="flex relative items-center">
                                         {index % 2 === 0 ? (
                                             <>
                                                 <ExperienceCard episode={episode} position="left" />
-                                                <div className="absolute left-1/2 transform -translate-x-1/2 w-4 h-4 bg-white rounded-full border-4 border-gray-900"></div>
+                                                <div className="absolute ml-0.5 lg:ml-0 lg:left-1/2 transform -translate-x-1/2 w-4 h-4 bg-white rounded-full border-4 border-gray-900"></div>
                                                 <div className="flex-1 pl-4"></div>
                                             </>
                                         ) : (
                                             <>
                                                 <div className="flex-1 pr-4"></div>
-                                                <div className="absolute left-1/2 transform -translate-x-1/2 w-4 h-4 bg-white rounded-full border-4 border-gray-900"></div>
+                                                <div className="absolute ml-0.5 lg:ml-0 lg:left-1/2 transform -translate-x-1/2 w-4 h-4 bg-white rounded-full border-4 border-gray-900"></div>
                                                 <ExperienceCard episode={episode} position="right" />
                                             </>
                                         )}
                                     </div>
+                                    
                                 ))}
                             </div>
                         )}
